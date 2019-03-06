f5a6e73ef607fcabfe05210b16409ecf6b0cd6bf
Niall Diamond <ndiamond@estechgroup.io>
2019-03-06 15:36:48 +0000

    Separate the config settings
    separate the config setting from the main js file. The config will be added into the footer block to allow the store owner to more easily make config chagnes.

---
+++ config.js
@@ -0,0 +1,18 @@
+// Configure your pop-in animations and delays 
+let myEvoXPopinTitle = 'Sale this Week',
+
+	promotionPopinDiscX = 'NewVisitor',
+
+	evoXAnimateParams =
+	{
+		element: '.myElement',
+		openAnimation: 'flipInX',
+		openDelay: 'delay-2s',
+	},
+
+	evoXAnimateCloseParams =
+	{
+		element: '.myElement',
+		closeAnimation: 'zoomOutDown',
+		//	closeDelay : '', 
+	};
